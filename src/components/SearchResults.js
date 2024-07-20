3e80a9c00d274f02d440c9d0ecf6e9c506a5877b
DaDevFox <mehul.tahiliani@gmail.com>
2024-07-20 12:13:53 -0400

    searchbar cleanup/refactoring

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,27 +3,37 @@ import { useState, useRef } from "react";
 import styles from "@/styles/page.module.css";
 
 export default function SearchResults({ query, sections }) {
-  const resultCount = 5;
-  const fuzzyCutoff = 5; // assuming levenshtein disance -- CHANGE IF ALGO CHANGES
   if (query == null || query == "")
     return (
       <div className={styles.searchResultDummy}>Type text to view results</div>
     );
 
+  const items = queryItems(query, sections);
+  return (
+    <div className={styles.searchResults}>
+      <ul>{items}</ul>
+    </div>
+  );
+}
+
+function queryItems(query, sections) {
   const idStr = (item) => item.course_name + " " + item.section_number;
 
-  const items = sections
+  const resultCount = 5;
+  const fuzzyCutoff = 5; // assuming levenshtein disance -- CHANGE IF ALGO CHANGES
+
+  return sections
     .sort((a, b) => {
       return fuzzyCompare(query, idStr(a)) > fuzzyCompare(query, idStr(b))
         ? 1
         : -1;
     })
     .slice(0, resultCount)
-    .map((item, index) => {
+    .map((item) => {
       var close = fuzzyCompare(query, idStr(item)) <= fuzzyCutoff;
 
       return (
-        <a key={index} href={item.link}>
+        <a key={idStr(item)} href={item.link}>
           <div
             className={`${close ? styles.searchResult_close : styles.searchResult_normal}`}
           >
@@ -32,10 +42,4 @@ export default function SearchResults({ query, sections }) {
         </a>
       );
     });
-
-  return (
-    <div className={styles.searchResults}>
-      <ul>{items}</ul>
-    </div>
-  );
 }
