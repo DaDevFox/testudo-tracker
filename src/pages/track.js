1092a670d1c01f0d16e2c122518515fd086e2f36
Justin Yang <111909590+JustinYang3@users.noreply.github.com>
2024-08-04 20:52:32 -0400

    Track Page pt2: progress bar + better button

--- src/pages/track.js
+++ src/pages/track.js
@@ -8,28 +8,144 @@ export default function Track(props) {
       course: "CMSC131",
       section: "0101",
       instructor: "Elias Gonzalez",
+      seats: 36,
+      seatsOpen: 3,
     },
-    { course: "CMSC131", section: "0102", instructor: "Elias Gonzalez" },
-    { course: "CMSC131", section: "0103", instructor: "Elias Gonzalez" },
-    { course: "CMSC131", section: "0104", instructor: "Elias Gonzalez" },
-    { course: "CMSC131", section: "0105", instructor: "Elias Gonzalez" },
-    { course: "CMSC131", section: "0201", instructor: "Pedram Sadeghian" },
-    { course: "CMSC131", section: "0202", instructor: "Pedram Sadeghian" },
-    { course: "CMSC131", section: "0203", instructor: "Pedram Sadeghian" },
-    { course: "CMSC131", section: "0204", instructor: "Pedram Sadeghian" },
-    { course: "CMSC131", section: "0205", instructor: "Pedram Sadeghian" },
+    {
+      course: "CMSC131",
+      section: "0102",
+      instructor: "Elias Gonzalez",
+      seats: 36,
+      seatsOpen: 9,
+    },
+    {
+      course: "CMSC131",
+      section: "0103",
+      instructor: "Elias Gonzalez",
+      seats: 36,
+      seatsOpen: 0,
+    },
+    {
+      course: "CMSC131",
+      section: "0104",
+      instructor: "Elias Gonzalez",
+      seats: 36,
+      seatsOpen: 12,
+    },
+    {
+      course: "CMSC131",
+      section: "0105",
+      instructor: "Elias Gonzalez",
+      seats: 36,
+      seatsOpen: 0,
+    },
+    {
+      course: "CMSC131",
+      section: "0201",
+      instructor: "Pedram Sadeghian",
+      seats: 36,
+      seatsOpen: 5,
+    },
+    {
+      course: "CMSC131",
+      section: "0202",
+      instructor: "Pedram Sadeghian",
+      seats: 36,
+      seatsOpen: 19,
+    },
+    {
+      course: "CMSC131",
+      section: "0203",
+      instructor: "Pedram Sadeghian",
+      seats: 36,
+      seatsOpen: 0,
+    },
+    {
+      course: "CMSC131",
+      section: "0204",
+      instructor: "Pedram Sadeghian",
+      seats: 36,
+      seatsOpen: 9,
+    },
+    {
+      course: "CMSC131",
+      section: "0205",
+      instructor: "Pedram Sadeghian",
+      seats: 36,
+      seatsOpen: 3,
+    },
   ];
   const CMSC132sections = [
-    { course: "CMSC132", section: "0101", instructor: "Larry Herman" },
-    { course: "CMSC132", section: "0102", instructor: "Larry Herman" },
-    { course: "CMSC132", section: "0103", instructor: "Larry Herman" },
-    { course: "CMSC132", section: "0104", instructor: "Larry Herman" },
-    { course: "CMSC132", section: "0105", instructor: "Larry Herman" },
-    { course: "CMSC132", section: "0201", instructor: "Nora Burkhauser" },
-    { course: "CMSC132", section: "0202", instructor: "Nora Burkhauser" },
-    { course: "CMSC132", section: "0203", instructor: "Nora Burkhauser" },
-    { course: "CMSC132", section: "0204", instructor: "Nora Burkhauser" },
-    { course: "CMSC132", section: "0205", instructor: "Nora Burkhauser" },
+    {
+      course: "CMSC132",
+      section: "0101",
+      instructor: "Larry Herman",
+      seats: 36,
+      seatsOpen: 2,
+    },
+    {
+      course: "CMSC132",
+      section: "0102",
+      instructor: "Larry Herman",
+      seats: 36,
+      seatsOpen: 1,
+    },
+    {
+      course: "CMSC132",
+      section: "0103",
+      instructor: "Larry Herman",
+      seats: 36,
+      seatsOpen: 10,
+    },
+    {
+      course: "CMSC132",
+      section: "0104",
+      instructor: "Larry Herman",
+      seats: 36,
+      seatsOpen: 0,
+    },
+    {
+      course: "CMSC132",
+      section: "0105",
+      instructor: "Larry Herman",
+      seats: 36,
+      seatsOpen: 15,
+    },
+    {
+      course: "CMSC132",
+      section: "0201",
+      instructor: "Nora Burkhauser",
+      seats: 36,
+      seatsOpen: 7,
+    },
+    {
+      course: "CMSC132",
+      section: "0202",
+      instructor: "Nora Burkhauser",
+      seats: 36,
+      seatsOpen: 4,
+    },
+    {
+      course: "CMSC132",
+      section: "0203",
+      instructor: "Nora Burkhauser",
+      seats: 36,
+      seatsOpen: 19,
+    },
+    {
+      course: "CMSC132",
+      section: "0204",
+      instructor: "Nora Burkhauser",
+      seats: 36,
+      seatsOpen: 8,
+    },
+    {
+      course: "CMSC132",
+      section: "0205",
+      instructor: "Nora Burkhauser",
+      seats: 36,
+      seatsOpen: 0,
+    },
   ];
 
   const CMSC131section_num = CMSC131sections.map((section) => (
@@ -37,7 +153,10 @@ export default function Track(props) {
       key={section.section}
       sectionNum={section.section}
       instructor={section.instructor}
-    ></Row>
+      seatsAval={(section.seats - section.seatsOpen) / section.seats}
+      seats={section.seats}
+      seatsOpen={section.seatsOpen}
+    />
   ));
 
   const CMSC132section_num = CMSC132sections.map((section) => (
@@ -45,7 +164,10 @@ export default function Track(props) {
       key={section.section}
       sectionNum={section.section}
       instructor={section.instructor}
-    ></Row>
+      seatsAval={(section.seats - section.seatsOpen) / section.seats}
+      seats={section.seats}
+      seatsOpen={section.seatsOpen}
+    />
   ));
 
   return (
@@ -57,10 +179,13 @@ export default function Track(props) {
   );
 }
 
-function Row({ sectionNum, instructor }) {
+function Row({ sectionNum, instructor, seatsAval, seats, seatsOpen }) {
   return (
     <div className="row">
       <div>{sectionNum}</div>
+      <progress value={seatsAval} className="progressBar" />
+      <div>{seatsOpen}</div>
+      <div>{seats}</div>
       <div>{instructor}</div>
       <Button />
     </div>
@@ -77,11 +202,23 @@ function Course({ children, course }) {
 }
 
 function Button() {
-  const [status, setStatus] = useState(false);
+  const [tracking, setTracking] = useState(false);
 
   return (
-    <button onClick={() => setStatus(!status)} className="button">
-      {`Tracking: ${status ? "Yes" : "No"}`}
+    <button
+      onClick={() => setTracking(!tracking)}
+      className={"button" + (tracking ? "Tracking" : "")}
+    >
+      {tracking ? "Tracking" : "Track"}
     </button>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <div>Section</div>
+      <div>Instructor</div>
+    </div>
   );
 }
