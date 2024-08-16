a754ff1e7868e4d6384c5a80b650ec15cc101ead
Justin Yang <111909590+JustinYang3@users.noreply.github.com>
2024-08-16 18:07:36 -0400

    basic login/register layout

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,22 +1,64 @@
-import "@/styles/globals.css";
-import styles from "@/styles/page.module.css";
+import styles from "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import "@/styles/login-page.css";
+
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase/config";
 
 export default function Login(props) {
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const login = (e) => {
+    e.preventDefault();
+    setError("");
+    signInWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        console.log(res.user);
+      })
+      .catch((error) => setError(error.message));
+    setEmail("");
+    setPassword("");
+  };
   return (
     <main className="main">
       <Navbar />
-      THIS IS THE LOGIN PAGE
+      <div className="login">
+        <form onSubmit={login}>
+          <h1>Log In</h1>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="button">
+            Log In
+          </button>
+          <p>
+            Already have an account?{" "}
+            <Link
+              href="/register"
+              style={{ color: "darkblue", textDecoration: "underline" }}
+            >
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
     </main>
   );
 }
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,23 +1,65 @@
 import styles from "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import "@/styles/register-page.css";
+
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase/config";
 
 export default function Register(props) {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const register = (e) => {
+    e.preventDefault();
+    setError("");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        console.log(res.user);
+      })
+      .catch((error) => setError(error.message));
+    setEmail("");
+    setPassword("");
+  };
 
   return (
     <main className="main">
       <Navbar />
-      THIS IS THE REGISTER PAGE
+      <div className="register">
+        <form onSubmit={register}>
+          <h1>Sign Up</h1>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="button">
+            Sign Up
+          </button>
+          <p>
+            Already have an account?{" "}
+            <Link
+              href="/login"
+              style={{ color: "darkblue", textDecoration: "underline" }}
+            >
+              Log in
+            </Link>
+          </p>
+        </form>
+      </div>
     </main>
   );
 }
