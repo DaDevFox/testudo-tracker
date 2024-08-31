11ece00ac0018038fb29472045074e2aa06ab2d5
Ji19283756 <ji19283756@gmail.com>
2024-08-31 13:59:08 -0400

    added more stuff for the forgot password page

--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -1,12 +1,22 @@
 import "@/styles/globals.css";
-import styles from "@/styles/page.module.css";
+import styles from "@/styles/forgot-password.module.css";
 import Navbar from "@/components/Navbar";
 
-export default function Login(props) {
+export default function Forgot(props) {
   return (
     <main className="main">
       <Navbar />
-      this is the forgot password pages
+      
+      <div className={styles.title_container}>
+        <h1>Forgot Password?</h1>
+        <h1>Enter username and we'll send you a reset link!</h1>
+        <h1>Username: <input></input> </h1> 
+      </div>
+
+
+
+
+        <br /> <br /> forgot password? <br /> go to hell!
     </main>
   );
 }
