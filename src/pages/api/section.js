5f86838fd605926f7682ce6aa7d795b890bca00a
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-08-31 15:23:56 -0400

    GET and POST section_index items

--- src/pages/api/section.js
+++ src/pages/api/section.js
@@ -20,9 +20,43 @@ export default async function handler(req, res) {
       // Choose a name for your collection
       const collection = database.collection("section-index");
 
-      await collection.insertOne({ data });
+      await collection.insertOne(data);
 
       res.status(201).json({ message: "Data saved successfully!" });
+    } catch (error) {
+      res.status(500).json({ message: `Something went wrong! ${error}` });
+    } finally {
+      await client.close();
+    }
+  } else if (req.method === "GET") {
+    // performs an EXACT match query
+
+    const client = new MongoClient(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    try {
+      console.log(req.body);
+      const { course_code, section_number } = req.body;
+      if (!course_code || !section_number) return;
+
+      await client.connect();
+
+      const database = client.db("testudo-index");
+      const collection = database.collection("section-index");
+
+      console.log("querying");
+      // constricting query to ONLY use course code and section num passed into this API
+      var result = await collection.findOne({
+        course_code: course_code,
+        section_number: section_number,
+      });
+
+      if (result) {
+        console.log("result: " + result);
+        res.status(200).json(result);
+      } else res.status(404);
     } catch (error) {
       res.status(500).json({ message: `Something went wrong! ${error}` });
     } finally {
---
+++ src/pages/api/services/tortoise.js
@@ -0,0 +1,3 @@
+export default async function update_index() {
+  var res = fetch("https://api.umd.io/v1/courses/sections");
+}
