73317e7faad08eb5ca909d91402de2358539a69d
Ji19283756 <ji19283756@gmail.com>
2024-08-31 13:34:11 -0400

    added framework for page

---
+++ src/pages/forgot-password.js
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import styles from "@/styles/page.module.css";
+import Navbar from "@/components/Navbar";
+
+export default function Login(props) {
+  return (
+    <main className="main">
+      <Navbar />
+      this is the forgot password pages
+    </main>
+  );
+}
