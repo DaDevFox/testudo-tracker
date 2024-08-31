b90a1178545c238c9cacea08ee406268c390e4b5
Justin Yang <111909590+JustinYang3@users.noreply.github.com>
2024-08-31 14:48:16 -0400

    fixed syntax errors

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -27,7 +27,6 @@ const Modal = ({ buttonName }) => {
             <div className={styles.modalCloseDiv} onClick={toggleModal}>
               <span>Close</span>
               <div className={styles.modalCloseBar}></div>
-
             </div>
 
             {/* Eventually, we should add a pop up when the class is successfully tracked */}
@@ -36,31 +35,12 @@ const Modal = ({ buttonName }) => {
               <div className={styles.modalTrackBar}></div>
             </div>
 
-        {modal && (
-                <div className={styles.modal}>
-                    <div className={styles.overlay} onClick={toggleModal}></div>
-
-                    <div className={styles.modalContent}>
-                        <div className={styles.modalCloseDiv} onClick={toggleModal}>
-                            <span>Close</span>
-                            <div className={styles.modalCloseBar}></div>
-                        </div>
-                        
-                        {/* Eventually, we should add a pop up when the class is successfully tracked */}
-                        <div className={styles.modalTrackDiv}>
-                            Track
-                            <div className={styles.modalTrackBar}></div>
-                        </div>
-                        
-                        <h2 className={styles.modalTtile}>Track this Class?</h2>
-                        <p className={styles.modalInfo}>{buttonName}</p>
-                        <p className={styles.modalInfo}>Proffessor: Lebron James</p>
-                        <p className={styles.modalInfo}>Timings: MWF 2:30-3:15 PM</p>
-                        <p className={styles.modalInfo}>Open Seats: 0, Waitlist: 0</p>
-
-                    </div>
-                </div>
-        )}
+            <h2 className={styles.modalTtile}>Track this Class?</h2>
+            <p className={styles.modalInfo}>{buttonName}</p>
+            <p className={styles.modalInfo}>Proffessor: Lebron James</p>
+            <p className={styles.modalInfo}>Timings: MWF 2:30-3:15 PM</p>
+            <p className={styles.modalInfo}>Open Seats: 0, Waitlist: 0</p>
+          </div>
         </div>
       )}
     </div>
