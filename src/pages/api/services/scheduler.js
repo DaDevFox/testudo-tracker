31fe5073415b47b42323c158849c6991299ed1ea
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-09-08 12:57:21 -0400

    Update GET api route for course_id structure

--- src/pages/api/services/scheduler.js
+++ src/pages/api/services/scheduler.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
 
 var cron = require("node-cron");
 
@@ -6,6 +7,10 @@ export default async function handler(req, res) {
   if (req.method == "POST") {
     try {
       cron.schedule("* * * * *", async () => {
+        var currPage = 0;
+        var pageSize = 50;
+        var currAmount = pageSize;
+
         console.log("");
         console.log("##################################");
         console.log("#                                #");
@@ -14,8 +19,30 @@ export default async function handler(req, res) {
         console.log("##################################");
         console.log("");
 
-        // Perform your action here
-        // TODO: link to python script as appropriate
+        // loop through pages, always at first to check if anything and until we hit the end
+        while (currPage == 0 || currAmount == pageSize) {
+          const response = await fetch(
+            `http://api.umd.io/v1/courses/sections?page=${currPage}&per_page=${30}`
+          );
+
+          console.log(`test`);
+          if (!response.ok) {
+            console.log(`Response status: ${response.status}`);
+            break;
+          }
+
+          const json = await response.json();
+          currAmount = json.length;
+
+          for (var i = 0; i < json.length; i++) {
+            var item = json[i];
+            console.log(`${item.section_id} --- checking`);
+            const response = await axios.get(
+              `${process.env.SERVER_URL}/api/section?course_id=${item.section_id}`
+            );
+            console.log(response.status);
+          }
+        }
       });
 
       res.status(200);
