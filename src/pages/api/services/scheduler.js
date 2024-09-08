026648af55379ca8a9f91643df9267bcb051d2ce
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-09-08 13:35:25 -0400

    Rolling index retreive + bulk add

--- src/pages/api/services/scheduler.js
+++ src/pages/api/services/scheduler.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { MongoClient } from "mongodb";
 import axios from "axios";
 
 var cron = require("node-cron");
@@ -6,44 +7,93 @@ var cron = require("node-cron");
 export default async function handler(req, res) {
   if (req.method == "POST") {
     try {
-      cron.schedule("* * * * *", async () => {
-        var currPage = 0;
-        var pageSize = 50;
-        var currAmount = pageSize;
+      // cron.schedule("* * * * *", async () => {
+      var currPage = 0;
+      var pageSize = 50;
+      var currAmount = pageSize;
 
-        console.log("");
-        console.log("##################################");
-        console.log("#                                #");
-        console.log("# Running scheduler every minute #");
-        console.log("#                                #");
-        console.log("##################################");
-        console.log("");
+      console.log("");
+      console.log("##################################");
+      console.log("#                                #");
+      console.log("# Running scheduler every minute #");
+      console.log("#                                #");
+      console.log("##################################");
+      console.log("");
 
-        // loop through pages, always at first to check if anything and until we hit the end
-        while (currPage == 0 || currAmount == pageSize) {
-          const response = await fetch(
-            `http://api.umd.io/v1/courses/sections?page=${currPage}&per_page=${30}`
-          );
+      const startTime = new Date();
+      const client = new MongoClient(process.env.MONGODB_URI, {});
 
-          console.log(`test`);
-          if (!response.ok) {
-            console.log(`Response status: ${response.status}`);
-            break;
-          }
+      // connect to Mongo client for read/write
+      await client.connect();
+      console.log("test");
 
-          const json = await response.json();
-          currAmount = json.length;
+      // FIRST retrieve all data from umd.io api
 
-          for (var i = 0; i < json.length; i++) {
-            var item = json[i];
-            console.log(`${item.section_id} --- checking`);
-            const response = await axios.get(
-              `${process.env.SERVER_URL}/api/section?course_id=${item.section_id}`
-            );
-            console.log(response.status);
-          }
+      const database = client.db("testudo-index");
+      const collection = database.collection("section-index");
+
+      var coursesToInsert = [];
+      var coursesSeen = {};
+
+      // loop through pages, always at first to check if anything and until we hit the end
+      while (currPage == 0 || currAmount == pageSize) {
+        const response = await fetch(
+          `http://api.umd.io/v1/courses/sections?page=${currPage}&per_page=${pageSize}`
+        );
+
+        if (!response.ok) {
+          console.log(`Response status: ${response.status}`);
+          break;
         }
-      });
+
+        console.log("received courses, deserializing");
+        const json = await response.json();
+        currAmount = json.length;
+
+        for (var i = 0; i < json.length; i++) {
+          var item = json[i];
+          console.log(`${item.section_id} --- checking`);
+
+          // check if in mongo db
+          var result = await collection.findOne({
+            course_id: item.section_id,
+          });
+
+          if (result == null) {
+            console.log("inserting");
+            coursesToInsert.push({
+              course_id: item.section_id,
+              professor: item.instructors[0],
+            });
+          } else {
+            console.log("exists");
+            coursesSeen[item.section_id] = {
+              course_id: item.section_id,
+              professor: item.instructors[0],
+            };
+          }
+          console.log("complete");
+        }
+
+        console.log("retreived " + currAmount);
+        currPage++;
+      }
+
+      const endTime = new Date();
+      console.log(
+        `Index api retrieve complete; elapsed: ${endTime - startTime}ms`
+      );
+
+      // SECOND update the mongo db persistent store with diff from data retrieved
+      if (coursesToInsert.length > 0) {
+        var result = await collection.insertMany(coursesToInsert);
+        if (result == null || result.insertedCount != coursesToInsert.length) {
+          console.log(`error; response received:${result}`);
+        }
+      }
+
+      client.close();
+      // });
 
       res.status(200);
     } catch (error) {
