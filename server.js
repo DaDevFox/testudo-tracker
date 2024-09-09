7e9c37329c2962f7b866f82d0af6bdb802c3647b
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-09-09 13:32:04 -0400

    test

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 import axios from "axios";
 import next from "next";
 
+const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,12 +12,6 @@ import { createServer } from "http";
 import { parse } from "url";
 
 app.prepare().then(async () => {
-  const port = process.env.PORT || 3000;
-  const httpServer = createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  });
-
   // cron trigger scheduler to scrape testudo
   const runScheduler = async () => {
     try {
@@ -33,7 +28,10 @@ app.prepare().then(async () => {
     }
   };
 
-  httpServer.listen(port, () => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, () => {
     console.log(
       `Testudo Tracker web server is running on http://localhost:${port}.\nWebscraper script is now being queried via the services/scheduler API route every minute.`
     );
