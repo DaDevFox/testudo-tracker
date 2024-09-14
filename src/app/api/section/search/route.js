36f5bcc7fedfe1b24913d2e00cc6d5ef6c3fe58a
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-09-14 19:46:53 -0400

    Create api/section/search GET for autocomplete search of mongo records

--- src/app/api/section/search/route.js
+++ src/app/api/section/search/route.js
@@ -1,13 +1,12 @@
 import { HttpStatusCode } from "axios";
 import { MongoClient } from "mongodb";
 
-export async function GET(request, res) {
-  if (request.method !== "GET") res.status(501).send();
+const SearchResultsMax = 100;
 
+export async function GET(request) {
+  const client = new MongoClient(process.env.MONGODB_URI, {});
   // performs a search with Atlas autocomplete query
   try {
-    const client = new MongoClient(process.env.MONGODB_URI, {});
-
     await client
       .connect()
       .catch((ex) => console.log(`mongodb connect failure ${ex}`));
@@ -16,42 +15,46 @@ export async function GET(request, res) {
     const database = client.db("testudo-index");
     const coll = database.collection("section-index");
 
-    const urlSearchParams = new URLSearchParams(
-      request.url.substring(request.url.indexOf("?") + 1)
-    );
-
-    if (!urlSearchParams)
-      return new Response(
-        "Requires query_string search parameter with string value"
-      );
-
-    const query_string = urlSearchParams.get("query_string");
+    const query_string = request.nextUrl.searchParams?.get("query_string");
+    var query_limit = request.nextUrl.searchParams?.get("query_limit");
 
     if (!query_string)
       return new Response(
         "Requires query_string search parameter with string value"
       );
 
+    console.log(query_string);
+    query_limit = query_limit || 10;
+    query_limit = Math.min(query_limit, SearchResultsMax);
+
     // define pipeline
     const agg = [
       {
         $search: {
           index: "default",
-          autocomplete: { query: query_string, path: "course_id" },
+          autocomplete: {
+            query: query_string,
+            path: "course_id",
+            fuzzy: { maxEdits: 2, prefixLength: 0, maxExpansions: 50 },
+          },
         },
       },
-      { $limit: 10 },
-      { $project: { _id: 0, title: 1 } },
+      { $limit: query_limit },
+      { $project: { _id: 0, course_id: 1, professor: 2 } },
     ];
 
     // run pipeline
     const result = await coll.aggregate(agg);
-    result.forEach((document) => {
-      console.log(document);
+    var resultJson = [];
+
+    await result.forEach((document) => {
+      resultJson.push(document);
     });
 
     // send results
-    return Response.json(result);
+    return new Response(JSON.stringify(resultJson), {
+      status: HttpStatusCode.Found,
+    });
   } catch (error) {
     console.log(error);
   } finally {
