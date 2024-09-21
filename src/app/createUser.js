a1d5e3ca6b7baa0084d9ea29308df3e3e948a4cd
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-09-21 12:44:34 -0400

    Add append user to user index on register

---
+++ src/app/createUser.js
@@ -0,0 +1,30 @@
+"use server";
+import axios from "axios";
+import { HttpStatusCode } from "axios";
+import { MongoClient } from "mongodb";
+
+// TODO: add custom server-sideMongoDB search code here to reduce api call overhead
+export async function addUser(email) {
+  const client = new MongoClient(process.env.MONGODB_URI, {});
+  // performs a search with Atlas autocomplete query
+  try {
+    await client
+      .connect()
+      .catch((ex) => console.log(`mongodb connect failure ${ex}`));
+
+    // set namespace
+    const database = client.db("testudo-index");
+
+    const collection = database.collection("dedicated-users");
+    const res = await collection.insertOne({
+      email: email,
+      watches: [],
+    });
+
+    console.log(`acknowledged: ${res.acknowledged}`);
+
+    return res;
+  } catch (ex) {
+    console.log(ex);
+  }
+}
