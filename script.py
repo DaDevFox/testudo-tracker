532318be4956f1298748585371b734fb556b0730
SBhima <emailshivank@gmail.com>
2024-09-21 22:47:43 -0400

    Changed the message of the email in the python file

--- script.py
+++ script.py
@@ -15,6 +15,7 @@ import pymongo
 # importing necessary functions from dotenv library
 from dotenv import load_dotenv, dotenv_values 
 import smtplib
+from email.mime.text import MIMEText
 # loading variables from .env file
 load_dotenv("./.env.local") 
 
@@ -88,9 +89,14 @@ def script_main():
                         smtpserver.login(YOUR_GOOGLE_EMAIL, YOUR_GOOGLE_EMAIL_APP_PASSWORD)                
                         sent_from = YOUR_GOOGLE_EMAIL
                         sent_to = recipient_email  
-                        email_text = str(prof.text) + " has " + str(open_seats.text) + " open seats in " + course_id + " in section " + section
-                        smtpserver.sendmail(sent_from, sent_to, email_text)
+                        email_text = course_id+" "+section+ " has " + str(open_seats.txt) + " with " +str(prof.text) + ". Sign up before the seats get taken at: app.testudo.umd.edu/main/dropAdd"
+                        message = MIMEText(email_text, "plain")
+                        message["Subject"] = str(open_seats.txt) + " open seats in " + course_id
+                        message["From"] = sent_from
+                        message["To"] = sent_to
+                        smtpserver.sendmail(sent_from, sent_to, message.as_string())
                         smtpserver.close()                
+                        print("Email sent to "+ recipient_email+ " for "+ course_id + " " + section)
 
 
 # Create and run the scheduler to run the function every time testudo updates
