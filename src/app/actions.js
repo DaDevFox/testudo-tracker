14f338b9abd2be780a5fc4b95728ea9c16ee1213
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-09-22 00:13:35 -0400

    Move server actions into actions.js

---
+++ src/app/actions.js
@@ -0,0 +1,66 @@
+"use server";
+import { MongoClient } from "mongodb";
+import { requestAutocompletes } from "./api/section/search/route";
+import { AppendToFastIndex, AppendToUserIndex } from "./api/track/route";
+
+export async function searchSections(query, limit) {
+  var client = new MongoClient(process.env.MONGODB_URI, {});
+  await client
+    .connect()
+    .catch((ex) => console.log(`MongoDB connect failure ${ex}`));
+
+  const database = client.db("testudo-index");
+  const collection = database.collection("section-index");
+
+  return await requestAutocompletes(collection, query, limit || 10);
+}
+
+export async function addUser(email) {
+  const client = new MongoClient(process.env.MONGODB_URI, {});
+  // performs a search with Atlas autocomplete query
+  try {
+    await client
+      .connect()
+      .catch((ex) => console.log(`mongodb connect failure ${ex}`));
+
+    // set namespace
+    const database = client.db("testudo-index");
+
+    const collection = database.collection("dedicated-users");
+    const res = await collection.insertOne({
+      email: email,
+      watches: [],
+    });
+
+    console.log(`acknowledged: ${res.acknowledged}`);
+
+    return res;
+  } catch (ex) {
+    console.log(ex);
+  }
+}
+
+export const trackCourse = async () => {
+  const email = "mtahilia@terpmail.umd.edu";
+  const course_id = "CMSC216-0101";
+  const client = new MongoClient(process.env.MONGODB_URI, {});
+  // performs a search with Atlas autocomplete query
+  try {
+    await client
+      .connect()
+      .catch((ex) => console.log(`mongodb connect failure ${ex}`));
+
+    const database = client.db("testudo-index");
+
+    var response = await AppendToFastIndex(database, course_id, email);
+    if (response) return response;
+
+    response = await AppendToUserIndex(database, course_id, email);
+    if (response) return response;
+
+    console.log(`acknowledged: ${res.acknowledged}`);
+    return null;
+  } catch (ex) {
+    console.log(ex);
+  }
+};
